199d89c0c20db3081dddb35e029c9848b42e472f
advaitkumar3107 <advaitkumar3107@github.com>
2021-09-03 21:19:09 +0530

    Added random connected graph generator

--- func.py
+++ func.py
@@ -1,23 +1,41 @@
-import obj
+from obj import node
 import param
 import random
+import numpy as np
 
 
 # Creates nodes in the network
-def createNetwork(num_nodes=10):
-    for i in range(num_nodes):
-        param.nodes[i] = (obj.node(i,20))
+def createNetwork(num_nodes=10, num_connections=12):
 
-    # Generate a connected graph through random sampling
-    for i in range(len(param.nodes)):
-        first_peer = random.randint(0,len(param.nodes)-1)
-        if first_peer == i:
-            first_peer = 0
-            if i == 0 and len(param.nodes)>1:
-                first_peer = 1
-        param.nodes[i].add_peer(param.nodes[first_peer])
-        param.nodes[first_peer].add_peer(param.nodes[i])
-        for j in range(i+1,len(param.nodes)):
-            if (random.random()<param.uniform_sampling_p):
-                param.nodes[i].add_peer(param.nodes[j])
-                param.nodes[j].add_peer(param.nodes[i])
+    for i in range(num_nodes):
+        param.nodes[i] = node(i,20)
+
+    # We first create a minimum spanning tree by just performing a random walk on the nodes
+    shuffled_indices = range(num_nodes)
+    random.shuffle(shuffled_indices)    # Shuffle node indices
+
+    adjacency_matrix = np.zeros((num_nodes, num_nodes))
+
+    np.fill_diagonal(adjacency_matrix, 1)
+
+    # Iterate over all the node indices
+    for index in range(num_nodes - 1):
+        param.nodes[shuffled_indices[index]].add_peer(param.nodes[shuffled_indices[index+1]]) # Add two consecutive nodes as
+        param.nodes[shuffled_indices[index+1]].add_peer(param.nodes[shuffled_indices[index]]) # neighbours according to shuffling
+        adjacency_matrix[shuffled_indices[index], shuffled_indices[index+1]] = 1    # Update edges in adjacency matrix
+        adjacency_matrix[shuffled_indices[index+1], shuffled_indices[index]] = 1
+
+    # These many edges are still left to be assigned
+    num_connections_left = num_connections - num_nodes + 1
+
+    while num_connections_left > 0:  # Till all edges havent been assigned
+        no_connection_rows, no_connection_cols = np.where(adjacency_matrix == 0)   # Edges that dont exist in the network
+        random_edge = random.choice(range(len(no_connection_rows)))
+        param.nodes[no_connection_rows[random_edge]].add_peer(param.nodes[no_connection_cols[random_edge]])
+        param.nodes[no_connection_cols[random_edge]].add_peer(param.nodes[no_connection_rows[random_edge]])
+        adjacency_matrix[no_connection_cols[random_edge], no_connection_rows[random_edge]] = 1
+        adjacency_matrix[no_connection_rows[random_edge], no_connection_cols[random_edge]] = 1
+
+        num_connections_left = num_connections_left - 1
+
+    return adjacency_matrix
--- obj.py
+++ obj.py
@@ -1,128 +1,127 @@
 import param
-import main
 import random
 
 
 # Node class to store each peer
 class node:
-	
-	# Initialize
-	def __init__(self,uniqueID,ia_time):
-		self.uniqueID = uniqueID
-		self.fast = (random.random()>main.parser.percent_slow)
-		self.ia_time = ia_time
-		self.pending_TXN = {}
-		self.peers = {}
-		self.balance = 0
 
-		# Blockchain stores a list of blocks validated by the node
-		# Key is the uniqueID of the block
-		# Value is a list of two elements:
-		# First is the distance from genesis block
-		# Second is the pointer to the block object for the block
-		# As the block has the ID of the previous block, we can create a chain from the block
-		self.blockchain = {}
-		self.blockchain[0] = [0,genesisBlock()]
-		self.longest = self.blockchain[0]
+    # Initialize
+    def __init__(self, uniqueID, ia_time):
+        self.uniqueID = uniqueID
+        self.fast = (random.random() > param.percent_slow)
+        self.ia_time = ia_time
+        self.pending_TXN = {}
+        self.peers = {}
+        self.balance = 0
 
-	# Handle to add new peer 
-	def add_peer(self,peer):
-		self.peers[peer.uniqueID] = [random.uniform(0.001,0.5),param.MB2b*(5+95*(self.fast and peer.fast))]
+        # Blockchain stores a list of blocks validated by the node
+        # Key is the uniqueID of the block
+        # Value is a list of two elements:
+        # First is the distance from genesis block
+        # Second is the pointer to the block object for the block
+        # As the block has the ID of the previous block, we can create a chain from the block
+        self.blockchain = {}
+        self.blockchain[0] = [0, genesisBlock()]
+        self.longest = self.blockchain[0]
 
-	# Generate a new transaction
-	def generateTransaction(self, payee_ID, amount, start_time):
+    # Handle to add new peer
+    def add_peer(self, peer):
+        self.peers[peer.uniqueID] = [random.uniform(0.001, 0.5), param.MB2b * (5 + 95 * (self.fast and peer.fast))]
 
-		# Compute time at which the TXN is generated (using exponetial distribution)
-		next_event_time = random.expovariate(1/self.ia_time) + start_time
+    # Generate a new transaction
+    def generateTransaction(self, payee_ID, amount, start_time):
 
-		# Unique TXN_ID
-		TXN_ID = param.next_TXN_ID
-		param.next_TXN_ID += 1
+        # Compute time at which the TXN is generated (using exponetial distribution)
+        next_event_time = random.expovariate(1 / self.ia_time) + start_time
 
-		# Add TXN generation to list of tasks
-		param.tasks[next_event_time] = str(TXN_ID) + ": " + str(self.uniqueID) + " pays " + str(payee_ID)\
-									   + " " + str(amount) + " coins"
-		
-		# Update own set of pending TXN
-		self.pending_TXN[TXN_ID] = param.tasks[next_event_time].split(": ",1)[1]
+        # Unique TXN_ID
+        TXN_ID = param.next_TXN_ID
+        param.next_TXN_ID += 1
 
-		# Broadcast to peers (omit no peers)
-		self.broadcastTransaction(param.tasks[next_event_time].split(": ",1)[1],next_event_time,[])
+        # Add TXN generation to list of tasks
+        param.tasks[next_event_time] = str(TXN_ID) + ": " + str(self.uniqueID) + " pays " + str(payee_ID) \
+                                       + " " + str(amount) + " coins"
 
-	# Broadcast the TXN
-	# Broadcasts to all the peers if a new TXN is generated
-	# If it is forwarded, the sender is omitted
-	def broadcastTransaction(self, TXN, start_time, peers_to_omit):
-		for peer in self.peers:
-			if (peer in peers_to_omit):
-				continue
-			param.tasks[self.peers[peer][0]+ param.TXN_size/self.peers[peer][1] +\
-			 random.expovariate(self.peers[peer][1]/(96*1024)) + start_time]\
-			 = "ReceiveTXN: "+str(self.uniqueID)+" "+str(peer)+" " + TXN
+        # Update own set of pending TXN
+        self.pending_TXN[TXN_ID] = param.tasks[next_event_time].split(": ", 1)[1]
 
-	def receiveTransaction(self, TXN, start_time, sender):
-		
-		# Proceed to broadcast if TXN not already received
-		# Else it would have been broadcasted earlier
-		if (int(TXN.split(":")[0]) not in self.pending_TXN):
-			self.pending_TXN[int(TXN.split(":")[0])] = TXN
-			self.broadcastTransaction( TXN, start_time, sender)
+        # Broadcast to peers (omit no peers)
+        self.broadcastTransaction(param.tasks[next_event_time].split(": ", 1)[1], next_event_time, [])
 
-			# Check whether the money in the TXN is for the receiver
-			if (int(TXN.split(" ")[3]) - self.uniqueID == 0):
-				self.balance += int(TXN.split(" ")[4])
+    # Broadcast the TXN
+    # Broadcasts to all the peers if a new TXN is generated
+    # If it is forwarded, the sender is omitted
+    def broadcastTransaction(self, TXN, start_time, peers_to_omit):
+        for peer in self.peers:
+            if (peer in peers_to_omit):
+                continue
+            param.tasks[self.peers[peer][0] + param.TXN_size / self.peers[peer][1] + \
+                        random.expovariate(self.peers[peer][1] / (96 * 1024)) + start_time] \
+                = "ReceiveTXN: " + str(self.uniqueID) + " " + str(peer) + " " + TXN
 
-	# Function to generate block
-	def generateBlock(self,start_time):
-		# Compute time at which the TXN is generated (using exponetial distribution)
-		# Copied from TXN part.  Edit mean creation time as suitable
-		next_event_time = random.expovariate(1/self.ia_time) + start_time
+    def receiveTransaction(self, TXN, start_time, sender):
 
-		new_block = block(self.longest[1])
-		param.blocks[new_block.uniqueID] = new_block
+        # Proceed to broadcast if TXN not already received
+        # Else it would have been broadcasted earlier
+        if (int(TXN.split(":")[0]) not in self.pending_TXN):
+            self.pending_TXN[int(TXN.split(":")[0])] = TXN
+            self.broadcastTransaction(TXN, start_time, sender)
 
-		# Add TXN generation to list of tasks
-		param.tasks[next_event_time] = "GenerateBlock: " + str(self.uniqueID) +" "+  str(new_block.uniqueID)
+            # Check whether the money in the TXN is for the receiver
+            if (int(TXN.split(" ")[3]) - self.uniqueID == 0):
+                self.balance += int(TXN.split(" ")[4])
 
-		# Broadcast to peers (omit no peers)
-		self.broadcastBlock(new_block,next_event_time,[])
-	
-	# Function to broadcast a block to peers
-	def broadcastBlock(self,block,start_time,peers_to_omit):
-		for peer in self.peers:
-			if (peer in peers_to_omit):
-				continue
-			param.tasks[self.peers[peer][0]+ param.Block_size/self.peers[peer][1] +\
-			 random.expovariate(self.peers[peer][1]/(96*1024)) + start_time]\
-			 = "ReceiveBlock: "+str(self.uniqueID)+" "+str(peer)+" "+str(block.uniqueID)
+    # Function to generate block
+    def generateBlock(self, start_time):
+        # Compute time at which the TXN is generated (using exponetial distribution)
+        # Copied from TXN part.  Edit mean creation time as suitable
+        next_event_time = random.expovariate(1 / self.ia_time) + start_time
 
-	# Function to receive block
-	# It will also validate the received block and broadcast further if valid
-	def receiveBlock(self):
-		pass
+        new_block = block(self.longest[1])
+        param.blocks[new_block.uniqueID] = new_block
 
-	def writeDataToFile(self):
-		file = open(param.file_prefix+str(self.uniqueID)+param.file_extension,"w")
-		for block in self.blockchain.values():
-			if (block[1].uniqueID == 0):
-				continue
-			else:
-				file.write(str(block[1].uniqueID)+" "+str(block[1].prev_block.uniqueID)+"\n")
-		file.close()
+        # Add TXN generation to list of tasks
+        param.tasks[next_event_time] = "GenerateBlock: " + str(self.uniqueID) + " " + str(new_block.uniqueID)
 
+        # Broadcast to peers (omit no peers)
+        self.broadcastBlock(new_block, next_event_time, [])
+
+    # Function to broadcast a block to peers
+    def broadcastBlock(self, block, start_time, peers_to_omit):
+        for peer in self.peers:
+            if (peer in peers_to_omit):
+                continue
+            param.tasks[self.peers[peer][0] + param.Block_size / self.peers[peer][1] + \
+                        random.expovariate(self.peers[peer][1] / (96 * 1024)) + start_time] \
+                = "ReceiveBlock: " + str(self.uniqueID) + " " + str(peer) + " " + str(block.uniqueID)
+
+    # Function to receive block
+    # It will also validate the received block and broadcast further if valid
+    def receiveBlock(self):
+        pass
+
+    def writeDataToFile(self):
+        file = open(param.file_prefix + str(self.uniqueID) + param.file_extension, "w")
+        for block in self.blockchain.values():
+            if (block[1].uniqueID == 0):
+                continue
+            else:
+                file.write(str(block[1].uniqueID) + " " + str(block[1].prev_block.uniqueID) + "\n")
+        file.close()
 
 
 # Object defining the structure of instance block
 # Add other methods/arguments as required
 class block:
 
-	def __init__(self, prev_block):
-		self.prev_block = prev_block
-		self.uniqueID = param.next_block_ID
-		param.next_block_ID += 1
+    def __init__(self, prev_block):
+        self.prev_block = prev_block
+        self.uniqueID = param.next_block_ID
+        param.next_block_ID += 1
+
 
 # Genesis block. All nodes start with this (see init function of node).
 class genesisBlock:
 
-	def __init__(self):
-		self.uniqueID = 0
+    def __init__(self):
+        self.uniqueID = 0
