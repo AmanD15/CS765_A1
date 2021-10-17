8f5948e3a89bdd7d438e7251c454c5b8687576b5
AmanD15 <amandhammani2@gmail.com>
2021-10-17 06:42:35 +0530

    Added state computation

--- selfish.py
+++ selfish.py
@@ -11,3 +11,13 @@ func.createNetwork()
 
 class selfish(node):
     def __init__(self):
+
+
+    def computeLeadState(self,has_attacker_generated):
+        if (self.private[0] - self.longest[0])>=2:
+            return str(self.private[0] - self.longest[0])
+        if (has_attacker_generated and (self.private[0] - self.longest[0])==1):
+            return "1"
+        if ((not has_attacker_generated) and (self.private[0] - self.longest[0])==1):
+            return "0'"
+        return "0"
